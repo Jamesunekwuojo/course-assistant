f893692be68833f4143ef93aa99b851331a340f4
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2026-07-15 00:37:13 +0100

    init coommit..

---
+++ README.md
@@ -0,0 +1,2 @@
+## MODULE 5 LLM ZOOMCAMP
+* Built ccourse chapt app, wrapped the RAG pipleine built earlier with streamlit.. 
---
+++ assistant.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from ingest import load_faq_data, build_index
+from rag_helper import RAGBase
+
+def create_assistant():
+    load_dotenv()
+
+    documents = load_faq_data()
+    index = build_index(documents)
+
+    MODEL="openai/gpt-oss-120b"
+
+    return RAGBase(
+        index=index,
+        llm_client=OpenAI(
+            base_url="https://api.groq.com/openai/v1",
+            api_key=os.getenv("GROQ_API_KEY")
+        ),
+    )
